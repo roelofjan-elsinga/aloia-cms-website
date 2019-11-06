ec760dfe4f7b76c41e7206025db2df5f9335e85d
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2019-11-06 11:37:44 +0100

    Added Tailwindcss as dependency

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        // Jekyll output directory
+        './_site/**/*.html',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+    plugins: [
+        require('tailwindcss')('./tailwindcss.config.js'),
+        require('cssnano')(),
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
+    ]
+};
